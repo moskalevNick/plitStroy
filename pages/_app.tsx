8a531460a5f85bd0ad79ddb29744a4dece9114f7
moskalevNick1 <n.moskalev@freshlimesoft.com>
2022-08-02 18:08:57 +0300

    add color for bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,7 +44,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
         <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff' />
+        <meta
+          name='theme-color'
+          content={`${({ theme }) => theme.colors.darkBlue}`}
+        />
       </Head>
       {loadComplete ? (
         <StyledThemeProvider theme={styledTheme}>
